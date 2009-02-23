d3b7e21fa2605fb08dfa3a0a4da8dfa4851d513c
Tim Connor <timocratic@gmail.com>
2009-02-23 13:19:17 -0800

    error swallowing is making it really hard to debug the install, if you are missing any other gems

--- config/requirements.rb
+++ config/requirements.rb
@@ -3,13 +3,13 @@ include FileUtils
 
 require 'rubygems'
 %w[rake hoe newgem rubigen].each do |req_gem|
-  begin
+  #begin
     require req_gem
-  rescue LoadError
-    puts "This Rakefile requires the '#{req_gem}' RubyGem."
-    puts "Installation: gem install #{req_gem} -y"
-    exit
-  end
+  #rescue LoadError
+  #  puts "This Rakefile requires the '#{req_gem}' RubyGem."
+  #  puts "Installation: gem install #{req_gem} -y"
+  #  exit
+  #end
 end
 
 $:.unshift(File.join(File.dirname(__FILE__), %w[.. lib]))
